8aeeffbaab788e1ebcf4244afb5e7dd1bc15d1db
Andrew Ackerman <andrew.ackers@gmail.com>
2020-01-30 09:03:27 -0800

    MVP

---
+++ users/users-router.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+
+const Users = require("./users-model.js");
+const restricted = require("../auth/restricted-middleware.js");
+
+router.get("/", restricted, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+// function onlyDepartment(department) {
+//   return function(req, res, next) {
+//     if (
+//       req.user &&
+//       req.user.department &&
+//       req.user.department.toLowerCase() === department
+//     ) {
+//       next();
+//     } else {
+//       res.status(403).json({ whoops: "that doesnt work" });
+//     }
+//   };
+// }
+
+module.exports = router;
